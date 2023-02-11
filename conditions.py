8824a9a0edfb8860172d57a3023b906af60a67ee
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 16:52:26 -0300

    Addition elif conditions, more conditions in our program

--- conditions.py
+++ conditions.py
@@ -5,12 +5,12 @@ hours = "hours"
 def function_to_calculation(number_of_days):
     if number_of_days > 0:
         return f"{number_of_days} days is {number_of_days * calculation_to_seconds} {hours}"
+    elif number_of_days = 0:
+        return "0 days have 0 hours"
     else:
         return "you have to enter a positive number"
 
 user_input = input("enter a number of day for a can convert in hours for you\n")
 
-user_input_int = int(user_input)
-
-resultofinput = function_to_calculation(user_input_int)
+resultofinput = function_to_calculation(int(user_input))
 print(resultofinput)
