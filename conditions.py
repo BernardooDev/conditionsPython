857773b7c8968e52b4f0d4392864f8b92ad5f936
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 15:22:15 -0300

    Putting if and else for conditions

--- conditions.py
+++ conditions.py
@@ -3,7 +3,10 @@ hours = "hours"
 
 
 def function_to_calculation(number_of_days):
-    return f"{number_of_days} dias são {number_of_days * calculation_to_seconds} {hours}"
+    if number_of_days > 0:
+        return f"{number_of_days} days is {number_of_days * calculation_to_seconds} {hours}"
+    else:
+        return "you have to enter a positive number"
 
 user_input = input("enter a number of day for a can convert in hours for you\n")
 
