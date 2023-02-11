988489273c6dfeb0dfd4251b3b7a2194c0c9db51
Bernardo Augusto <bernardo.almeida@uscsonline.com.br>
2023-02-11 16:59:27 -0300

    Using the function .isdigit in the variable

--- conditions.py
+++ conditions.py
@@ -5,12 +5,15 @@ hours = "hours"
 def function_to_calculation(number_of_days):
     if number_of_days > 0:
         return f"{number_of_days} days is {number_of_days * calculation_to_seconds} {hours}"
-    elif number_of_days = 0:
+    elif number_of_days == 0:
         return "0 days have 0 hours"
     else:
         return "you have to enter a positive number"
 
 user_input = input("enter a number of day for a can convert in hours for you\n")
 
-resultofinput = function_to_calculation(int(user_input))
-print(resultofinput)
+if user_input.isdigit():
+    resultofinput = function_to_calculation(int(user_input))
+    print(resultofinput)
+else:
+    print("You need to put a number, not a string")
